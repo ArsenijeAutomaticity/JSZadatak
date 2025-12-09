6289fe75f461911793e468a9fe22dfbb86c1b48f
ArsenijeAutomaticity <arsenije.knezevic@automaticity.rs>
2025-12-09 12:00:58 +0100

    Function name change

--- Day4/zad7.js
+++ Day4/zad7.js
@@ -1,4 +1,4 @@
-function isInRange(arrayN, a, b) {
+function isInRangeSort(arrayN, a, b) {
   if (arrayN.length === 0) return "no elements";
   arrayN.sort((a, b) => a - b);
   if (arrayN[0] < a || arrayN[arrayN.length - 1] > b) return false;
@@ -7,4 +7,4 @@ function isInRange(arrayN, a, b) {
 
 let nums = [22, 6, 7, 3, 5, 1];
 
-console.log(isInRange(nums, 1, 66));
+console.log(isInRangeSort(nums, 1, 66));
